d50783548e4c6d528eaf96b0a0918da77ea836aa
Asami <doiasami1219@gmail.com>
2023-02-21 08:13:20 +0000

    Succeed to run a x86 binary on qemu-system-x86_64 but text doesn't show

--- gui/default/src/main.rs
+++ gui/default/src/main.rs
@@ -8,30 +8,22 @@ use core::ptr::null_mut;
 
 extern crate alloc;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    //let vga_buffer = 0xb8000 as *mut u8;
+    let vga_buffer = 0xa0000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
-
-/*
-macro_rules! entry_point {
-    // c.f. https://docs.rs/bootloader/0.6.4/bootloader/macro.entry_point.html
-    ($path:path) => {
-        #[no_mangle]
-        pub unsafe extern "C" fn entry() -> i64 {
-            // validate the signature of the program entry point
-            let f: fn() -> i64 = $path;
-            f()
-        }
-    };
-}
-
-entry_point!(main);
-
-fn main() -> i64 {
-    return -42;
-}
-*/
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
--- renderer/src/lib.rs
+++ renderer/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(alloc_error_handler)]
+
 #[macro_use]
 extern crate alloc;
 
