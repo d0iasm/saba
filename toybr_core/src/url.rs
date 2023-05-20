a74bd44cfd627f956f240101d661c89cd17a0a54
Asami Doi <doiasami1219@gmail.com>
2023-05-20 14:35:22 +0200

    clean up unused code

--- toybr_core/src/url.rs
+++ toybr_core/src/url.rs
@@ -5,42 +5,6 @@ use alloc::string::String;
 use alloc::string::ToString;
 use alloc::vec::Vec;
 
-/*
-#[derive(Debug, Clone, PartialEq)]
-enum Protocol {
-    Http,
-    Https,
-}
-
-impl Protocol {
-    fn to_string(&self) -> String {
-        match self {
-            Protocol::Http => String::from("http"),
-            Protocol::Https => String::from("https"),
-        }
-    }
-
-    /// Default port numbers are defined by Internet Assigned Numbers Authority (IANA).
-    /// https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml
-    fn default_port_number(&self) -> u16 {
-        match self {
-            // https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml?&page=2
-            Protocol::Http => 80,
-            //
-            Protocol::Https => 443,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct ParsedUrl {
-    _scheme: Protocol,
-    pub host: String,
-    pub port: u16,
-    pub path: String,
-}
-*/
-
 #[derive(Debug, Clone, PartialEq)]
 pub struct ParsedUrl {
     scheme: String,
@@ -50,83 +14,6 @@ pub struct ParsedUrl {
 }
 
 impl ParsedUrl {
-    /*
-    fn extract_scheme(url: &String) -> Protocol {
-        let splitted_url: Vec<&str> = url.split("://").collect();
-        if splitted_url.len() == 2 && splitted_url[0] == Protocol::Http.to_string() {
-            Protocol::Http
-        } else if splitted_url.len() == 2 && splitted_url[0] == Protocol::Https.to_string() {
-            Protocol::Https
-        } else if splitted_url.len() == 1 {
-            // No scheme. Set "HTTP" as a default behavior.
-            Protocol::Http
-        } else {
-            panic!("unsupported scheme: {}", url);
-        }
-    }
-
-    fn remove_scheme(url: &String, scheme: &Protocol) -> String {
-        // Remove "scheme://" from url if any.
-        url.replacen(&(scheme.to_string() + "://"), "", 1)
-    }
-
-    fn extract_host(url: &String) -> String {
-        let splitted_url: Vec<&str> = url.splitn(2, '/').collect();
-        let host_and_port: Vec<&str> = splitted_url[0].splitn(2, ':').collect();
-        host_and_port[0].to_string()
-    }
-
-    fn extract_path(url: &String) -> Option<String> {
-        let splitted_url: Vec<&str> = url.splitn(2, '/').collect();
-        if splitted_url.len() == 2 {
-            Some(splitted_url[1].to_string())
-        } else {
-            None
-        }
-    }
-
-    fn extract_port(url: &String) -> Option<u16> {
-        let splitted_url: Vec<&str> = url.splitn(2, '/').collect();
-        let host_and_port: Vec<&str> = splitted_url[0].splitn(2, ':').collect();
-        if host_and_port.len() == 2 {
-            Some(host_and_port[1].parse::<u16>().unwrap())
-        } else {
-            None
-        }
-    }
-
-    pub fn new(original_url: String) -> Self {
-        // HTTP format
-        // http://<host>:<port>/<path>?<searchpart>
-        //
-        // https://datatracker.ietf.org/doc/html/rfc1738#section-3.3
-        //
-        // possible format:
-        // https://url.spec.whatwg.org/#urls
-
-        let scheme = Self::extract_scheme(&original_url);
-        let url = Self::remove_scheme(&original_url, &scheme);
-
-        let host = Self::extract_host(&url);
-        let path = match Self::extract_path(&url) {
-            Some(p) => p,
-            None => String::new(),
-        };
-
-        let port = match Self::extract_port(&url) {
-            Some(h) => h,
-            None => scheme.default_port_number(),
-        };
-
-        Self {
-            _scheme: scheme,
-            host,
-            port,
-            path,
-        }
-    }
-    */
-
     pub fn new(url: String) -> Self {
         // HTTP format
         // http://<host>:<port>/<path>?<searchpart>
@@ -172,68 +59,6 @@ impl ParsedUrl {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    /*
-    #[test]
-    fn test_url1() {
-        let url = "http://example.com".to_string();
-        let expected = ParsedUrl {
-            _scheme: Protocol::Http,
-            host: "example.com".to_string(),
-            port: 80,
-            path: "".to_string(),
-        };
-        assert_eq!(expected, ParsedUrl::new(url));
-    }
-
-    #[test]
-    fn test_url2() {
-        let url = "http://example.com:8888".to_string();
-        let expected = ParsedUrl {
-            _scheme: Protocol::Http,
-            host: "example.com".to_string(),
-            port: 8888,
-            path: "".to_string(),
-        };
-        assert_eq!(expected, ParsedUrl::new(url));
-    }
-
-    #[test]
-    fn test_url3() {
-        let url = "http://example.com:8888/index.html".to_string();
-        let expected = ParsedUrl {
-            _scheme: Protocol::Http,
-            host: "example.com".to_string(),
-            port: 8888,
-            path: "index.html".to_string(),
-        };
-        assert_eq!(expected, ParsedUrl::new(url));
-    }
-
-    #[test]
-    fn test_url4() {
-        let url = "example.com:8888/index.html".to_string();
-        let expected = ParsedUrl {
-            _scheme: Protocol::Http,
-            host: "example.com".to_string(),
-            port: 8888,
-            path: "index.html".to_string(),
-        };
-        assert_eq!(expected, ParsedUrl::new(url));
-    }
-
-    #[test]
-    fn test_localhost() {
-        let url = "localhost:8000".to_string();
-        let expected = ParsedUrl {
-            _scheme: Protocol::Http,
-            host: "localhost".to_string(),
-            port: 8000,
-            path: "".to_string(),
-        };
-        assert_eq!(expected, ParsedUrl::new(url));
-    }
-    */
 
     #[test]
     fn test_url1() {
