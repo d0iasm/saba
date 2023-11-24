a1451624c4126f69f88093192d1e87e97c9b8a00
Asami Doi <doiasami1219@gmail.com>
2023-11-24 09:12:25 +0100

    add handle_url that returns the same http response

--- main/main.rs
+++ main/main.rs
@@ -4,11 +4,18 @@
 
 extern crate alloc;
 
+use crate::alloc::string::ToString;
 use alloc::alloc::GlobalAlloc;
 use alloc::alloc::Layout;
 use alloc::rc::Rc;
+use alloc::string::String;
+use alloc::vec::Vec;
 use core::cell::RefCell;
 use noli::*;
+use toybr_core::browser::Browser;
+use toybr_core::error::Error;
+use toybr_core::http::HttpResponse;
+use toybr_core::renderer::page::Page;
 use toybr_core::ui::UiObject;
 use ui_wasabi::WasabiUI;
 
@@ -34,13 +41,33 @@ fn my_allocator_error(_layout: Layout) -> ! {
 #[global_allocator]
 static GLOBAL_ALLOCATOR: Allocator = Allocator;
 
+fn handle_url<U: UiObject>(url: String) -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::new(
+        "1.1".to_string(),
+        200,
+        "".to_string(),
+        Vec::new(),
+        "<h1>example</h1>".to_string(),
+    ))
+}
+
 fn main() -> u64 {
     sys_print("**** Hello from an app!\n");
 
     // initialize the UI object
     let ui = Rc::new(RefCell::new(WasabiUI::new()));
+    let page = Rc::new(RefCell::new(Page::new()));
 
-    sys_exit(42);
+    // initialize the main browesr struct
+    let browser = Rc::new(RefCell::new(Browser::new(ui.clone(), page.clone())));
+
+    match ui.borrow_mut().start(handle_url::<WasabiUI>) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("browser fails to start {:?}", e);
+            sys_exit(1);
+        }
+    };
     0
 }
 
