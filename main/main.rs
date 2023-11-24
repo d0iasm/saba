546c697e79463eca5091db20637d1ae43a00fbfb
Asami Doi <doiasami1219@gmail.com>
2023-11-24 09:03:58 +0100

    add a global allocator that does nothing

--- main/main.rs
+++ main/main.rs
@@ -1,10 +1,45 @@
 #![no_std]
 #![no_main]
+#![feature(alloc_error_handler)]
 
+extern crate alloc;
+
+use alloc::alloc::GlobalAlloc;
+use alloc::alloc::Layout;
+use alloc::rc::Rc;
+use core::cell::RefCell;
 use noli::*;
+use toybr_core::ui::UiObject;
+use ui_wasabi::WasabiUI;
+
+#[derive(Default)]
+pub struct Allocator;
+
+// TODO: implement allocator
+unsafe impl GlobalAlloc for Allocator {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        core::ptr::null_mut()
+        //malloc(layout.size() as u32) as *mut u8
+    }
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        //free(ptr as *mut c_void);
+    }
+}
+
+#[alloc_error_handler]
+fn my_allocator_error(_layout: Layout) -> ! {
+    panic!("out of memory");
+}
+
+#[global_allocator]
+static GLOBAL_ALLOCATOR: Allocator = Allocator;
 
 fn main() -> u64 {
     sys_print("**** Hello from an app!\n");
+
+    // initialize the UI object
+    let ui = Rc::new(RefCell::new(WasabiUI::new()));
+
     sys_exit(42);
     0
 }
--- ui/wasabi/src/lib.rs
+++ ui/wasabi/src/lib.rs
@@ -1,1 +1,33 @@
 #![no_std]
+
+extern crate alloc;
+
+use alloc::rc::Weak;
+use alloc::string::String;
+use core::cell::RefCell;
+use toybr_core::{browser::Browser, error::Error, http::HttpResponse, ui::UiObject};
+
+#[derive(Clone, Debug)]
+pub struct WasabiUI {
+    browser: Weak<RefCell<Browser<Self>>>,
+    input_url: String,
+}
+
+impl UiObject for WasabiUI {
+    fn new() -> Self {
+        Self {
+            browser: Weak::new(),
+            input_url: String::new(),
+        }
+    }
+
+    fn console_debug(&mut self, log: String) {}
+    fn console_warning(&mut self, log: String) {}
+    fn console_error(&mut self, log: String) {}
+    fn start(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    ) -> Result<(), Error> {
+        Ok(())
+    }
+}
